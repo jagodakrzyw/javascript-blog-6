21f073b612b2fcf84db097c752b9f1271d091944
Jagoda Krzywdzińska <jagodakrzywdzinska@gmail.com>
2022-09-03 12:29:00 +0200

    Add const for href attribute

--- js/script.js
+++ js/script.js
@@ -6,9 +6,10 @@
 }); */
 
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
-
+    
     /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
@@ -19,7 +20,7 @@ const titleClickHandler = function(event){
 
     /* [DONE] add class 'active' to the clicked link */
 
-    clickedElement.classList.add('active') 
+    clickedElement.classList.add('active'); 
     
     console.log('clickedElement:', clickedElement);
 
@@ -31,7 +32,10 @@ const titleClickHandler = function(event){
         activeArticle.classList.remove('active');
     }
 
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
+
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
 
     /* find the correct article using the selector (value of 'href' attribute) */
 
