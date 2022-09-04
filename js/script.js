940a37c2211ac9512aa79d579661ac1e1ebac772
Jagoda Krzywdzińska <jagodakrzywdzinska@gmail.com>
2022-09-04 12:16:48 +0200

    Install EditorConfig and StyleLint and update task runner

--- js/script.js
+++ js/script.js
@@ -6,40 +6,40 @@
 }); */
 
 const titleClickHandler = function(event){
-    event.preventDefault();
-    const clickedElement = this;
-    
-    /* [DONE] remove class 'active' from all article links  */
+  event.preventDefault();
+  const clickedElement = this;
 
-    const activeLinks = document.querySelectorAll('.titles a.active');
+  /* [DONE] remove class 'active' from all article links  */
 
-    for(let activeLink of activeLinks){
-        activeLink.classList.remove('active');
-    }
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
-    /* [DONE] add class 'active' to the clicked link */
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
 
-    clickedElement.classList.add('active'); 
+  /* [DONE] add class 'active' to the clicked link */
 
-    /* [DONE] remove class 'active' from all articles */
+  clickedElement.classList.add('active');
 
-    const activeArticles = document.querySelectorAll('.post.active');
+  /* [DONE] remove class 'active' from all articles */
 
-    for(let activeArticle of activeArticles){
-        activeArticle.classList.remove('active');
-    }
+  const activeArticles = document.querySelectorAll('.post.active');
 
-    /* [DONE] get 'href' attribute from the clicked link */
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
 
-    const articleSelector = clickedElement.getAttribute('href');
+  /* [DONE] get 'href' attribute from the clicked link */
 
-    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+  const articleSelector = clickedElement.getAttribute('href');
 
-    const targetArticle = document.querySelector(articleSelector);
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
 
-    /* [DONE] add class 'active' to the correct article */
+  const targetArticle = document.querySelector(articleSelector);
 
-    targetArticle.classList.add('active'); 
+  /* [DONE] add class 'active' to the correct article */
+
+  targetArticle.classList.add('active');
 }
 
 const optArticleSelector = '.post',
@@ -48,38 +48,38 @@ const optArticleSelector = '.post',
 
 function generateTitleLinks(){
 
-    /* [DONE] remove contents of titleList */
+  /* [DONE] remove contents of titleList */
 
-    const titleList = document.querySelector(optTitleListSelector);
-    titleList.innerHTML = '';
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
 
-    /* [DONE] find all the articles and save them to variable: articles */
+  /* [DONE] find all the articles and save them to variable: articles */
 
-    const articles = document.querySelectorAll(optArticleSelector);
+  const articles = document.querySelectorAll(optArticleSelector);
 
-    let html = '';
+  let html = '';
 
-    for (let article of articles){
+  for (let article of articles){
 
-        /* [DONE] get the article id */
+    /* [DONE] get the article id */
 
-        const articleId = article.getAttribute('id');
+    const articleId = article.getAttribute('id');
 
-        /* [DONE] find the title element */
-        /* [DONE] get the title from the title element */
+    /* [DONE] find the title element */
+    /* [DONE] get the title from the title element */
 
-        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
-        /* [DONE] create HTML of the link */
+    /* [DONE] create HTML of the link */
 
-        const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
 
-        /* [DONE] insert link into titleList */
+    /* [DONE] insert link into titleList */
 
-        html = html + linkHTML;
-    }
+    html = html + linkHTML;
+  }
 
-    titleList.innerHTML = html;
+  titleList.innerHTML = html;
 }
 
 generateTitleLinks();
