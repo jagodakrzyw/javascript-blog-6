31fd433155986fb192ce028d55b293a6663db04e
Jagoda Krzywdzińska <jagodakrzywdzinska@gmail.com>
2022-09-04 13:17:53 +0200

    Add a start loop for each tag

--- js/script.js
+++ js/script.js
@@ -115,19 +115,25 @@ function generateTags(){
 
     /* split tags into array */
 
-    
+    const articleTagsArray = articleTags.split(' ');
+    console.log(articleTagsArray);
 
     /* START LOOP: for each tag */
 
+    for(let tag of articleTagsArray){
+
       /* generate HTML of the link */
+
+      
 
       /* add generated code to html variable */
 
     /* END LOOP: for each tag */
-  }
+    }
     /* insert HTML of all the links into the tags wrapper */
 
   /* END LOOP: for every article: */
+  }
 }
 
 generateTags();
